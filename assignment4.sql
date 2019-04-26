ebbb44f3294db870c5eaae4d34e343e859e703d9
James <ellerbee_james1@columbusstate.edu>
2019-04-25 20:49:10 -0400

    initial project version

---
+++ assignment4.sql
@@ -0,0 +1,15 @@
+/* 
+assignment 4, do one stored procedure that does the following
+has to read from at least 3 tables (to be done on the employees table) has to:
+1 add a table
+2 instert into a table
+3 update a table
+4 delete a record from a table
+5 delete a table
+6 do an inner join
+7 have an if else satements
+8 return a value
+9 code a trigger on each three ops: Update, insert, delete
+*/
+
+-- Question: Create a table called salairesAndtitle. Insert each title into the table. Update it with the lowest salary and the emp_no that holds the lowest salaray for each title.
