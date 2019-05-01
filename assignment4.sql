a5dc613159113e7bc508eee202f90d5bf4c328ad
Woods-Taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-05-01 00:39:32 -0500

    update comments

--- assignment4.sql
+++ assignment4.sql
@@ -1,8 +1,8 @@
 /*
 assignment 4, do one stored procedure that does the following
 has to read from at least 3 tables (to be done on the employees table) has to:
-1 add a table
-2 instert into a table
+1 add a table - add table of percentage salary increases by department
+2 instert into a table - calculate salary increase from percentage and use for updating
 3 update a table
 4 delete a record from a table
 5 delete a table
@@ -20,13 +20,21 @@ has to read from at least 3 tables (to be done on the employees table) has to:
 -- Delete: when a new percent pay increase table is created delete old percent insertions
 -- Insert: when a new percent pay increase table is created insert percentages
 -- return sum of pay increases to clarify, the salary increase should be calculated based on percentages and then inserted into the appropriate department table
+
 delimiter //
 CREATE PROCEDURE sum_pay_increase ()
 BEGIN
 DROP TABLE IF EXISTS empSalary;
---4
+-- 4
 DELETE FROM salary
 WHERE to_date = "1987-06-28";
+
+-- 7
+
+
+
+-- 9
+
 -- 1 create table
 CREATE TABLE empSalary
 (
@@ -35,7 +43,7 @@ CREATE TABLE empSalary
     salary INT,
 
     PRIMARY KEY(emp_no)
-);
+); 
 -- 2 insert into table
 INSERT INTO empSalary(emp_no)
 SELECT emp_no
@@ -51,9 +59,12 @@ INSERT INTO empSalary(title)
 SELECT title
 FROM titles
 WHERE empSalary.emp_no = titles.title;
---5
+-- 5
 DROP TABLE IF EXISTS salary;
---6
---what tables do you want in a union?
+-- 6
+-- what tables do you want in a union?
+
+-- 8
+
 END//
 delimiter ;
